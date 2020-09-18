3194abf3906f6f50c4595dddab345a5f1bfd679d
Lauriacunia <Lauriacunia@gmail.com>
2020-09-18 20:46:56 -0300

    Empiezo a cambiar el nombre de las clases para hacerlas mas descriptivas segun su funcion y a reestructurar el maquetado de toda la web

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu-responsive-icon = document.querySelector('.menu-responsive-icon');
+
+
+const navResponsive = document.querySelector('.header-navbar');
+
+
+
+menu-responsive-icon.onclick = () => {
+
+navResponsive.classList.toggle("hidde-menu-header")
+
+
+}

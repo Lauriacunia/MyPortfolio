03d6123303f6ae2370d114aae1ef5d4161f56f01
Lauriacunia <Lauriacunia@gmail.com>
2020-09-22 20:23:40 -0300

    Termino menu de filtros responsive

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const headerMenuIcon = document.querySelector('.menu-responsive-icon');
 const navbarResponsive = document.querySelector('.header-navbar-responsive');
 
+const menuFilterIcon = document.querySelector('.menu-filter-icon');
+const filterResponsive = document.querySelector('.menu-filter-responsive');
 
 
+// Funcionalidad del Boton Menu Header
 headerMenuIcon.onclick = () => {
 
 navbarResponsive.classList.toggle("header-navbar-is-hidden")
 
 }
+
+// Funcionalidad del Boton Filtro Mis Proyectos
+
+menuFilterIcon.onclick = () => {
+
+    filterResponsive.classList.toggle("filter-navbar-is-hidden")
+    
+    }

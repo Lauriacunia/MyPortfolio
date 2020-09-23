7fd23d456dbaffaa6aadaac5d89eac49c877a415
Lauriacunia <Lauriacunia@gmail.com>
2020-09-23 13:27:39 -0300

    Termino de agregar funcionalidad a los filtros de proyectos

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const navbarResponsive = document.querySelector('.header-navbar-responsive');
 const menuFilterIcon = document.querySelector('.menu-filter-icon');
 const filterResponsive = document.querySelector('.menu-filter-responsive');
 
+const botonesFiltros = document.querySelectorAll('.btn-filter');
+console.log(botonesFiltros)
+const projectsCards = document.querySelectorAll('.card-project');
+console.log(projectsCards)
+
+
+
+
+const filterCards = (btnFilter) =>{
+    const filterCategory = btnFilter.value;
+    console.log(filterCategory)
+    
+    for (let projectCard of projectsCards){
+
+        const cardCategory = projectCard.dataset.category 
+        console.log(cardCategory)
+
+        if (cardCategory == filterCategory || filterCategory == "todos"){
+            console.log(cardCategory == filterCategory)
+            projectCard.classList.remove("hidde");
+        }else {
+            projectCard.classList.add("hidde");
+            
+        }
+    }
+}
+
+// ----INICIALIZADORES ------//
 
 // Funcionalidad del Boton Menu Header
 headerMenuIcon.onclick = () => {
@@ -18,4 +46,10 @@ menuFilterIcon.onclick = () => {
 
     filterResponsive.classList.toggle("filter-navbar-is-hidden")
     
+}
+
+for (let btnFilter of botonesFiltros){
+    btnFilter.onclick = () => {
+        filterCards(btnFilter);
     }
+}
